3876ad1c8d7b202e983c462748259807ec39c4a0
Darrius-W <darriuswhite435@gmail.com>
2022-08-18 20:40:20 -0500

    added comments

--- rng.py
+++ rng.py
@@ -10,6 +10,7 @@ while True:
     print('Password Generator')
     print('-'*18)
 
+    # get user requested pwd length
     try:
         pwdLength = int(input('Enter length for you password(max 100): '))
         if pwdLength > 100:
@@ -21,12 +22,14 @@ while True:
 
     generatedPwd = ''
 
+    # create user pwd with requested length
     for x in range(pwdLength):
         generatedPwd += random.choice(chars)
 
     print('\nYour Generated Password is: ' + generatedPwd)
     
-    genNewPwd = input('\nWould you like to generate a new password?(y/n):')
+    # see if user would like to generate new pwd
+    genNewPwd = input('\nWould you like to generate a new password?(y/n): ')
         
     if genNewPwd == 'y':
         continue
