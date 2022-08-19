b8c9cc0c3d6fa02336f6f4015ecede936af7c1af
Darrius-W <darriuswhite435@gmail.com>
2022-08-18 19:22:25 -0500

    user can now generate multiple passwords

--- rng.py
+++ rng.py
@@ -5,13 +5,20 @@ import random
 # all valid characters for creating pwd
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!()-.?[]_`~;:@#$%^&*+='
 
-print('Password Generator')
-print('-'*18)
+while True:
 
-pwdLength = int(input('\nEnter length for you password: '))
-generatedPwd = ''
+    print('Password Generator')
+    print('-'*18)
 
-for x in range(pwdLength):
-    generatedPwd += random.choice(chars)
+    pwdLength = int(input('\nEnter length for you password: '))
+    generatedPwd = ''
 
-print('\nYour Generated Password is: ' + generatedPwd)
+    for x in range(pwdLength):
+        generatedPwd += random.choice(chars)
+
+    print('\nYour Generated Password is: ' + generatedPwd)
+    genNewPwd = input('\nWould you like generate a new password?(y/n):')
+    
+    if genNewPwd == 'n':
+        print('Goodbye')
+        break
